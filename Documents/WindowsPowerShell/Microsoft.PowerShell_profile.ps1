8e8a3db10f822bb42832c3772925d99c242bdda3
denco <dlochman@sygic.com>
2022-08-14 12:32:08 +0200

    Initial commit

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+$ENV:STARSHIP_CONFIG = "$HOME\AppData\Local\starship\starship.toml"
+
+Invoke-Expression (&starship init powershell)
+
+function dtf([string]$testName)
+{
+    dotnet test --filter "FullyQualifiedName~$testName"
+}
+
+function nvimdf()
+{
+    git --git-dir="$HOME/AppData/Local" --work-tree="$HOME" $Args
+}
+
+function dotfiles()
+{
+    git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" $Args
+}
